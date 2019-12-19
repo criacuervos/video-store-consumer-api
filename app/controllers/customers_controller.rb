4a23f6aa0c12d3e133ffde8cc4f53cf8f99aaae6
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-12-18 16:39:53 -0800

    Added new customer create function

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,17 +10,28 @@ class CustomersController < ApplicationController
       data = Customer.all
     end
     
-    data = data.paginate(page: params[:p], per_page: params[:n])
+    # data = data.paginate(page: params[:p], per_page: params[:n])
     
     render json: data.as_json(
       only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :account_credit],
       methods: [:movies_checked_out_count]
     )
   end
+  
   def show
     customer = Customer.find(params[:id])
     render json: customer.as_json
   end
+  
+  def create 
+    customer = Customer.new(name: params[:name], address: params[:address], city: params[:city], state: params[:state], postal_code: params[:postal_code], phone: params[:phone], registered_at: Date.today, account_credit: 0.0)
+    
+    if customer.save 
+      render status: :ok, json: { customer: customer }
+    else 
+      render status: :bad_request, json: { errors: customer.errors.messages }
+    end 
+  end 
   
   private
   def parse_query_args
