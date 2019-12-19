ce259effc079d5f3aa59bf4c14a2ae0832d51055
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-12-19 14:40:51 -0800

    Non-working Rental List

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,40 +1,40 @@
 class MoviesController < ApplicationController
   before_action :require_movie, only: [:show]
-
+  
   def index
     if params[:query]
       data = MovieWrapper.search(params[:query])
     else
       data = Movie.all
     end
-
+    
     render status: :ok, json: data
   end
-
+  
   def create 
     movie = Movie.new(title: params[:title], overview: params[:overview], release_date: params[:release_date], inventory: [:inventory])
-
+    
     if movie.save 
       render status: :ok, json: {}
     else 
       render status: :bad_request, json: { errors: movie.errors.messages }
     end 
   end 
-
+  
   def show
     render(
       status: :ok,
       json: @movie.as_json(
         only: [:title, :overview, :release_date, :inventory],
         methods: [:available_inventory]
-        )
       )
+    )
   end
-
+  
   private
-
+  
   def require_movie
-    @movie = Movie.find_by(title: params[:title])
+    @movie = Movie.find_by(title: params[:title]) || Movie.find_by(id: params[:id])
     unless @movie
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
